10a1b8d6693b2f3abd28658eb918bcf008e93053
ss900371tw <52847269+ss900371tw@users.noreply.github.com>
2025-06-21 15:10:00 +0800

    Update pic.py

--- pic.py
+++ pic.py
@@ -33,7 +33,7 @@ import sys
 from bs4 import BeautifulSoup
 import time
 import undetected_chromedriver as uc
-from google_search_results  import GoogleSearch
+from serpapi.google_serch import GoogleSearch
 
 
 load_dotenv()
