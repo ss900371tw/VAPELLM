f8531532c503bef4026e812243b41090f21ca44a
ss900371tw <52847269+ss900371tw@users.noreply.github.com>
2025-07-07 16:06:18 +0800

    Update pic.py

--- pic.py
+++ pic.py
@@ -754,7 +754,7 @@ def main():
             st.markdown(f"""
             <div id="{key}_card">
                 <div style="font-size: 1.8rem;">{icon}</div>
-                <div style="font-size: 1.8rem; font-weight: bold;">{title}</div>
+                <div style="font-size: 1.6rem; font-weight: bold;">{title}</div>
                 <div style="font-size: 1.4rem; color: #ccc;">{desc}</div>
                 <div style="margin-top: 10px;">
             """, unsafe_allow_html=True)
