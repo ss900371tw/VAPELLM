114ef059c10632b5b0aaeea010b477ac21c7a1ec
ss900371tw <52847269+ss900371tw@users.noreply.github.com>
2025-07-07 16:05:36 +0800

    Update pic.py

--- pic.py
+++ pic.py
@@ -753,9 +753,9 @@ def main():
             # ✅ 把所有內容寫在這一個 HTML block 裡
             st.markdown(f"""
             <div id="{key}_card">
-                <div style="font-size: 1.4rem;">{icon}</div>
-                <div style="font-size: 2.0rem; font-weight: bold;">{title}</div>
-                <div style="font-size: 1.8rem; color: #ccc;">{desc}</div>
+                <div style="font-size: 1.8rem;">{icon}</div>
+                <div style="font-size: 1.8rem; font-weight: bold;">{title}</div>
+                <div style="font-size: 1.4rem; color: #ccc;">{desc}</div>
                 <div style="margin-top: 10px;">
             """, unsafe_allow_html=True)
     
