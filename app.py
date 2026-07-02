1fa1f25a900fca4d39f73b9fe47b346183b9657c
ss900371tw <52847269+ss900371tw@users.noreply.github.com>
2026-07-02 18:25:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1386,7 +1386,7 @@ span[data-testid="uploaded-file-name"] {
 </style>
 """, unsafe_allow_html=True)
             uploaded_files = st.file_uploader(
-                "", type=["jpg", "jpeg", "png"], accept_multiple_files=True, label_visibility="collapsed"
+                "Upload Images", type=["jpg", "jpeg", "png"], accept_multiple_files=True, label_visibility="collapsed"
             )
         
             # 如果有上傳新圖片 → 重置狀態
